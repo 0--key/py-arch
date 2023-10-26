b8f60e6c21e62ca673440c7ad0e84e58ffc9ba2f
Anton S. Kosinov <a.s.kosinov@gmail.com>
2023-10-26 10:47:16 +0100

    A folder as a module approach for code and tests

---
+++ tests/test_arbitrary_variable.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+from bunch.a_sample_variable import a
+
+
+class ArbitraryContentPrimitiveTest(unittest.TestCase):
+    """Does import works as expected?"""
+
+    def test_variable_vaue(self):
+        self.assertEqual(a, "An arbitrary string")
